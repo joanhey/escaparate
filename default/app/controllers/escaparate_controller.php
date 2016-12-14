32680b405e4a340d7ec18ca8b3594d2e6655ab4e
Joan Miquel <joanhey@kumbiaphp.com>
2016-12-14 09:39:46 +0100

    Mejoras del código
    Falta cambiar los métodos del modelo a camelCase, mejorar el Input de agregar, es necesario el View::template() ??.
    Posiblemente los métodos del modelo que sólo devuelven datos, podrían ser estáticos ( Web::dameTodasPaginadas(),  Web::find() ), no es necesario instanciar un modelo si solamente devuelve instancias.

--- default/app/controllers/escaparate_controller.php
+++ default/app/controllers/escaparate_controller.php
@@ -3,36 +3,33 @@
 class EscaparateController extends AppController {
 
     function before_filter() {
-        View::template('proto');
+        View::template('proto'); //Realmente es necesario ??
     }
 
     function index($page = 1) {
-        $this->listado = (new Web())->dame_todas_paginadas($page);
+        $this->listado = (new Web)->dame_todas_paginadas($page);
     }
 
     function agregar() {
         if (Input::hasPost('web', 'autor')) {
             if ((new Site)->addNew(Input::post('web'), Input::post('autor'), Input::post('social'), $_FILES['imagenweb'])) {
-                Flash::valid(_('Sitio agregado exitosamente.'));
+                Flash::valid(_('Sitio agregado exitosamente. Gracias.'));
                 return Redirect::to();
-            } else {
-                Flash::error(_('Se ha producido un error inesperado.'));
             }
+            Flash::error(_('Error al añadir website.'));
         }
     }
 
     function autorizar($id = null)
     {
-      if ($id == null) {
-        $this->listado = (new Web)->find("conditions: activa = 0"); // elementos pendientes
-      } else {
-        if ((new Web)->activar($id)) {
-      		Flash::valid(_('Sitio fue activado exitosamente'));
-          return Redirect::to();
-      	}
-      }
-
-
+          if ($id == null) {
+            $this->listado = (new Web)->find("conditions: activa = 0"); // elementos pendientes
+            return;
+          }
+          if ((new Web)->activar($id)) {
+                Flash::valid(_('Sitio activado exitosamente'));
+                return Redirect::to();
+          }
+          Flash::error(_('Error al activar website.'));
     }
-
 }
